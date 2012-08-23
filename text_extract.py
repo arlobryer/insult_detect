1c70591affcba0a765dcfaabd8beee67afbbcbd8
arlogb <arlobryer@gmail.com>
2012-08-23 10:11:31 +0100

    Some modification to text extraction. Doesn't improve result to remove stopwords.

--- text_extract.py
+++ text_extract.py
@@ -1,12 +1,14 @@
 import nltk
+from nltk.corpus import stopwords
 
 wanted = ['PRP$', 'PRP', 'NN', 'NNS', 'VB', 'VBD',
           'VBZ', 'VBG', 'VBP', 'VBN', 'JJ', 'JJR', 'JJS', 
-          'MOD', 'UH', '!']
+          'UH', '!']
 #list of tags for Part-of-speech at:
 #http://www.ling.upenn.edu/courses/Fall_2003/ling001/penn_treebank_pos.html
 
-grammar = 'NP: {<DT>?<JJ>*<NN>}'
+grammar = r"""NP: {<JJ>*<NN>}
+           {<PRP>}"""
 
 def get_words(com):
     w = nltk.word_tokenize(com)
@@ -22,25 +24,35 @@ def get_words(com):
 
 def get_wlist(w_list):
     wordlist = [w[0].lower() for w in w_list]
+    # clean_wordlist = stopword_clean(wordlist, stopwords.words('english'))
     w_type_list = nltk.FreqDist(nltk.pos_tag(wordlist))
     return w_type_list
 
 #http://nltk.googlecode.com/svn/trunk/doc/book/ch07.html
 
 def info_extract(com):
-    w = nltk.sent_tokenize(com)
-    w = [nltk.word_tokenize(sent) for sent in w]
-    w = [nltk.pos_tag(sent) for sent in w]
+    sent = nltk.sent_tokenize(com)
+    words = [nltk.word_tokenize(s) for s in sent]
+    w_pos = [nltk.pos_tag(w) for w in words]
+    # print w_pos
     out = []
-    for t in w:
-        if (t[1] in wanted):
-            out.append(t)
+    for c in w_pos:
+        for t in c:
+            if (t[1] in wanted):
+                out.append(t)
+    # print chunker(grammar, out)
     return out
+
 
 def chunker(grammar, sent):
     parser = nltk.RegexpParser(grammar)
     res = parser.parse(sent)
-    res.draw()
+    # res.draw()
     return res
+
+def stopword_clean(l, stops):
+    l2 = [w for w in l if w not in stops]
+    return l2
+
     
     
